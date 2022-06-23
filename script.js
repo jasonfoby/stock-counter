2ce9f7f171fbb4ea08dea596cb5fde7bdb11fd1b
jasonfoby <69209806+jasonfoby@users.noreply.github.com>
2022-06-24 04:07:00 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+/*
+Dom Sammut 2013
+***************
+Web: www.domsammut.com
+Twitter: www.twitter.com/domsammut
+
+View codepen on my website: https://www.domsammut.com/projects/pure-css-loading-animation
+
+*/
